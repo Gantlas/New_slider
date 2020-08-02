d21618d020d959fd50d126a94376ebe620d5aed1
Gantlas <67436576+Gantlas@users.noreply.github.com>
2020-08-02 16:29:45 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,48 +1,54 @@
+const showSlide = (index) => {
+  const slides = document.querySelectorAll(".item");
+  const dots = document.querySelectorAll(".dots-item");
+
+  switch (index) {
+    case slides.length:
+      slideIndex = 0;
+      break;
+    case -1:
+      slideIndex = slides.length - 1;
+      break;
+  }
+
+  slides.forEach((slide, i) => {
+    if (i === slideIndex) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+
+  dots.forEach((dot, i) => {
+    if (i === slideIndex) {
+      dot.style.backgroundColor = "lightgray";
+    } else {
+      dot.style.backgroundColor = "transparent";
+    }
+  });
+};
+
 let slideIndex = 0;
 showSlide(slideIndex);
 
-prev = document.querySelector(".prev");
+const prev = document.querySelector(".prev");
 
 prev.onclick = () => {
   --slideIndex;
   showSlide(slideIndex);
 };
 
-next = document.querySelector(".next");
+const next = document.querySelector(".next");
 
 next.onclick = () => {
   ++slideIndex;
   showSlide(slideIndex);
 };
 
-dots = document.querySelectorAll(".dots-item");
-dots.forEach((dot, i, arr) => {
+const dots = document.querySelectorAll(".dots-item");
+dots.forEach((dot, i) => {
   dot.onclick = () => {
     slideIndex = i;
     showSlide(i);
   };
 });
-
-function showSlide(index) {
-  const slides = document.querySelectorAll(".item");
-  const dots = document.querySelectorAll(".dots-item");
-
-  if (index > slides.length - 1) {
-    slideIndex = 0;
-  }
-
-  if (index < 0) {
-    slideIndex = slides.length - 1;
-  }
-
-  slides.forEach((slide) => {
-    slide.style.display = "none";
-  });
-
-  dots.forEach((dot) => {
-    dot.style.backgroundColor = "transparent";
-  });
-
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].style.backgroundColor = "lightgray";
-}
